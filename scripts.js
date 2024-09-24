33e81a98c49d71c79369afd54fb5f0b8052c3320
Mayur Kharade <kharade.mayur@gmail.com>
2024-09-24 14:22:14 +0530

    Simple setup of basic website

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you shortly.');
+  });
+  
